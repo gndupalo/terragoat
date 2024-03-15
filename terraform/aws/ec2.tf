e1f785f7a9c2215bff1229f34425b4b2d871a4aa
gndupalo <113545731+gndupalo@users.noreply.github.com>
2024-03-15 05:42:58 -0400

    Update ec2.tf
    Updated to see hardfail

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_host" {
-  # ec2 have plain text secrets in user data . Made a recent update in comments by Gndu
+  # ec2 have plain text secrets in user data . Made a recent update in comments by Gndu to see hard fails in action
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
